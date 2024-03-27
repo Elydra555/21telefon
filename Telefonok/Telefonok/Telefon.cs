4536078d5d4d27b409dff1edc175faae4df53904
Elydra555 <165134282+Elydra555@users.noreply.github.com>
2024-03-27 09:49:15 +0100

    Add Telefonok

---
+++ Telefonok/Telefonok/Telefon.cs
@@ -0,0 +1,27 @@
+﻿namespace Telefonok
+{
+    public class Telefon
+    {
+        public string Gyarto;
+        public string Modell;
+        public string Kiadas;
+        public int Akkap;
+
+        public Telefon(string v)
+        {
+        }
+
+        public Telefon(string gyarto, string modell, string kiadas, int akkap)
+        {
+            Gyarto = gyarto;
+            Modell = modell;
+            Kiadas = kiadas;
+            Akkap = akkap;
+        }
+
+        public override string? ToString()
+        {
+            return $"{Modell} telefon vagyok, {Gyarto} a gyártóm, {Kiadas} adtak ki, és {Akkap} az akkumlátorom kapacitása.";
+        }
+    }
+}
---
+++ Telefonok/Telok/Program.cs
@@ -0,0 +1,7 @@
+﻿using Telefonok;
+
+
+Telefon t = new Telefon("Huawei", "P20", "2015-07-08", 4000);
+
+
+Console.WriteLine(t);
