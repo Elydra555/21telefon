c794de7cdd129cc9545d307ca5360d642adbe4f1
Elydra555 <165134282+Elydra555@users.noreply.github.com>
2024-03-27 09:50:14 +0100

    Add 21csv

---
+++ 21csv/ClassLibrary/alapDolgok/Data/EmberekContext.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary.Data
+{
+    public class EmberekContext : DbContext
+    {
+        public EmberekContext() { }
+
+        public DbSet<Emberek> Emberkek { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //string connMsSQL = "Server=(localdb)\\mssqllocaldb;Database=API.Data;MultipleActiveResultSets=true";
+            string connMySQL = "Server=127.0.0.1;uid=root;passwd=;Database=nyilvantartas;";
+            optionsBuilder.UseMySql(connMySQL, ServerVersion.AutoDetect(connMySQL));
+
+            //optionsBuilder.UseMySql(connMsSQL);
+        }
+    }
+}
---
+++ 21csv/ClassLibrary/alapDolgok/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary.Data;
+using ClassLibrary.Models;
+
+namespace alapDolgok
+{
+    public class Program
+    {
+        static void Kiir(IEnumerable<object> szemelyek)
+        {
+            foreach (var item in szemelyek)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            EmberekContext db = new EmberekContext();
+
+            if (!db.Emberkek.Any())
+            {
+                var sorok = File.ReadAllLines(@"C:\Users\Diak\dev\kb\asztali\oop\21telefon\21csv\21.csv").Skip(1);
+                foreach (var line in sorok)
+                {
+                    db.Emberkek.Add(new Emberek(line));
+                }
+                db.SaveChanges();
+            }
+            Kiir(db.Emberkek);
+        }
+    }
+}
